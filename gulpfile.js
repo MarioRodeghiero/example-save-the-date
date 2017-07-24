4006c3a1130bc2d222330d12140d4d43d7ce1b39
MarioRodeghiero <mario.rodeghiero@gmail.com>
2017-07-23 22:35:26 -0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
-var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin');
-    clean = require('gulp-clean');
+var gulp = require('gulp')
+    ,imagemin = require('gulp-imagemin')//npm install gulp-imagemin --save-dev
+    ,clean = require('gulp-clean')//npm install gulp-clean --save-dev
+    ,concat = require('gulp-concat')//npm install gulp-concat --save-dev
+    ,htmlReplace = require('gulp-html-replace')//npm install gulp-html-replace --save-dev
+    ,usemin = require('gulp-usemin')//npm install gulp-usemin --save-dev
+    ,uglify = require('gulp-uglify')//npm install gulp-uglify --save-dev
+    ,cssmin = require('gulp-cssmin')//npm install gulp-cssmin --save-dev
+    ,browserSync = require('browser-sync')//npm install browser-sync --save-dev
+    ,jshint = require('gulp-jshint')//npm install jshint gulp-jshint --save-dev
+    ,jshintStylish = require('jshint-stylish')//npm install --save-dev jshint-stylish
+    ,csslint = require('gulp-csslint')//npm install --save-dev gulp-csslint
+    ,sass = require('gulp-sass');
 
 // tarefa para criar a pasta dist com todos arquivos
-gulp.task('copy',['clean'], function() {
+gulp.task('copy', ['clean'], function() {
 
     return gulp.src('src/**/*') // retorna uma stream dizendo que finalizou a tarefa "assincrona"
         .pipe(gulp.dest('dist'));
@@ -18,18 +28,69 @@ gulp.task('clean', function( ) {
 
 
 // Tarefa para reduzir imagens
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
 
     gulp.src('dist/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
 });
 
-// // Static server
-// gulp.task('browser-sync', function() {
-//     browserSync.init({
-//         server: {
-//             baseDir: "./"
-//         }
-//     });
-// });
+
+// Tarefa que aplica o uglify e mimifica os arquivos css e js, onde apontados no arquivo html
+gulp.task('usemin', function() {
+  
+    gulp.src('dist/**/*.html')
+    .pipe(usemin({
+        js: [ uglify()],
+        css: [ cssmin() ]
+        // html: [ htmlmin({ collapseWhitespace: true }) ],
+        // inlinejs: [ uglify() ],
+        // inlinecss: [ cleanCss(), 'concat' ]
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass', function() {
+
+    return gulp.src('src/sass/**/*.scss')
+       .pipe(sass().on('error', sass.logError))
+       .pipe(autoprefixer('last 4 version'))
+       .pipe(gulp.dest('src/css'));
+}); 
+
+//Tarefa para atualizar o browser
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+
+    gulp.watch('src/**/*').on('change', browserSync.reload);
+
+    gulp.watch('src/js/**/*.js').on('change', function(event) { // verifica erros nos arquivos .js
+        console.log("Linting " + event.path);
+        gulp.src(event.path)
+            .pipe(jshint())
+            .pipe(jshint.reporter(jshintStylish));
+
+    });
+    
+    gulp.watch("src/sass/**/*.scss", ['sass']);
+
+    gulp.watch('src/css/**/*.css').on('change', function(event) { // verifica erros nos arquivos .js
+        console.log("Linting " + event.path);
+        gulp.src(event.path)
+            .pipe(csslint())
+            .pipe(csslint.formatter());
+
+    });
+});
+
+
+
+//Tarefa default
+gulp.task('default', ['copy'], function() {
+
+    gulp.start('build-img', 'usemin');
+});
